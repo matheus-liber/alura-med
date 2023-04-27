32fb8521facad1a66ea22cd02418639dbc83962b
matheus-liber <matheuslibergt@gmail.com>
2023-04-27 15:48:09 -0300

    Módulo 1 e 2 concluídos

---
+++ src/componentes/Carrossel/index.js
@@ -0,0 +1,48 @@
+import React, { useEffect, useRef, useState } from "react";
+import { View, FlatList, Image } from "react-native";
+import styles from "./styles";
+
+export function Carrossel({ data, tempoCarrossel }){
+    const carrosselRef = useRef();
+    const [indice, setIndice] = useState(0);
+
+    function alteraPosicaoObjeto() {
+        if(indice < data.length - 1){
+            setIndice(indice + 1);
+        } else {
+            setIndice(0);
+        }
+    }
+
+    useEffect(() => {
+        carrosselRef.current.scrollToIndex({ index: indice })
+
+        const intervalo = setInterval(() => {
+            alteraPosicaoObjeto();
+        }, tempoCarrossel)
+
+        return () => clearInterval(intervalo);
+    }, [indice])
+    
+    
+    return(
+        <View style={styles.container}>
+            <FlatList
+              data={data}
+              horizontal
+              showsHorizontalScrollIndicator={false}
+              keyExtractor={item => item.id}
+              renderItem={({ item, index }) => (
+                <Image
+                  source={item.imagem}
+                  style={[styles.image, 
+                    index == data.length - 1 ? {marginRight: 200} : null
+                    ]}
+                  resizeMode="contain"
+                />
+              )}
+              ref={carrosselRef}
+            />
+        </View>
+    )
+}
